f4a1e9f8b6b02760ad6f97ef96109cc420fbc6c1
karl mogensen <karlmogensen@hotmail.com>
2020-03-16 12:41:07 +0100

    foundation

--- GitTest/Foundations/FUserFoundation.cs
+++ GitTest/Foundations/FUserFoundation.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GitTest.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,44 @@ namespace GitTest.Foundations
 {
     class FUserFoundation
     {
-        List<>
+        List<User> users = new List<User> { new User(1, "karl"), new User(2, "nichlas"), new User(3, "martin") };
+
+        public List<User> GetAll()
+        {
+            return users;
+        }
+
+        public void Create(User userToCreate)
+        {
+            if (!ContainsUser(userToCreate))
+                users.Add(userToCreate);
+            else
+                throw new FormatException("user already exists");
+        }
+        public void Update(User userToUpdate)
+        {
+            //get 
+        }
+
+        public User GetById(int id)
+        {
+            foreach (User user in users)
+                if (user.Id == id)
+                    return user;
+
+
+            return null;
+        }
+
+        private bool ContainsUser(User user)
+        {
+            foreach(User u in users)
+            {
+                if (u.Id == user.Id)
+                    return true;
+            }
+            return false;
+        }
+
     }
 }
