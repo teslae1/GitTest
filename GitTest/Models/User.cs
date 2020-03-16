2a62b2423eb011b22307bd0515439deb87ebe3fc
karl mogensen <karlmogensen@hotmail.com>
2020-03-16 12:54:29 +0100

    Models update User class

--- GitTest/Models/User.cs
+++ GitTest/Models/User.cs
@@ -4,8 +4,10 @@ using System.Text;
 
 namespace GitTest.Models
 {
+    //karl
     class User
     {
+        public static int incrementer = 100;
         public int Id { get; private set; }
         public string Name { get; private set; }
         public User(int id, string name)
@@ -13,7 +15,19 @@ namespace GitTest.Models
             Id = id;
             Name = name;
         }
-
         
+
+        /// <summary>
+        /// auto increments user_Id
+        /// </summary>
+        /// <param name="name"></param>
+        public User(string name)
+        {
+            Name = name;
+            Id = incrementer++;
+        }
+        
+
+
     }
 }
