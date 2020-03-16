f1f4fd6c3200716d5477d11e3d9544153c82ed9a
karl mogensen <karlmogensen@hotmail.com>
2020-03-16 12:31:01 +0100

    user class

---
+++ GitTest/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitTest.Models
+{
+    class User
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public User(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        
+    }
+}
